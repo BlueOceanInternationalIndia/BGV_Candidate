4b0772e362777807804efec6610a8c4fa0e0f84c
SaarthakBatra <saarthak.batra@gmail.com>
2024-07-12 14:24:47 +0530

    Auth Server Updated

--- public/scripts/config/config.js
+++
@@ -1,7 +0,0 @@
-export const MAX_File_SIZE_MB = 2;
-
-export const DATA_SERVER_URI = 'https://bgv-candidate-dataserver.onrender.com';
-// export const DATA_SERVER_URI = 'http://localhost:7777';
-
-export const AUTH_SERVER_URI = 'https://bgv-candidate-authserver.onrender.com';
-// export const AUTH_SERVER_URI = 'http://localhost:5555';
